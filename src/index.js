f870d3f89dd3a95a0ddc2e6875a72de77adcce22
ash213-gif <ashish789pa@gmail.com>
2026-03-09 10:17:09 +0530

    .

--- src/index.js
+++ src/index.js
@@ -24,10 +24,18 @@ mongoose.connect(process.env.MongoUrl)
 
 // Routes
 app.use('/', routes);
+
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  credentials: true
+  origin: [
+    process.env.FRONTEND_URL,
+    "http://localhost:5173",
+  ],
+  credentials: true,
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"]
 }));
+// app.options(/.*/, cors());
 
 initSocket(server);
 
