76d5eb552dffde5cc6bdf4d2e3691fe04a43a5fc
ash213-gif <ashish789pa@gmail.com>
2026-03-09 10:01:52 +0530

    ..

--- src/Redis/Socket.js
+++ src/Redis/Socket.js
@@ -7,7 +7,8 @@ const initSocket = (server) => {
 
   io = new Server(server, {
     cors: {
-      origin: process.env.frontened_url  ||"http://localhost:5173/",
+      origin: process.env.FRONTEND_URL   ||"http://localhost:5173",
+      credentials: true,
     },
   });
 
--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 const { initSocket } = require('./Redis/Socket');
 const Port = 3030;
 
@@ -25,12 +24,10 @@ mongoose.connect(process.env.MongoUrl)
 
 // Routes
 app.use('/', routes);
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://myapp.vercel.app");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true
+}));
 
 initSocket(server);
 
